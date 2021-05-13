e12a5d4457e89e5bd7330d03c493163d63b86a44
Deepmoy Banerjee <75161113+coderdeep31@users.noreply.github.com>
2021-05-13 10:42:43 +0530

    Add files via upload

---
+++ Variable Assignment #1.py
@@ -0,0 +1,16 @@
+""" ------------Variables Assignment------------------
+This file is describes the attributes and details about my favorite song
+
+"""
+
+artist = 'Arijit Singh' # Singer of the song
+genre = 'Pop' # Genre of the song
+song = 'Sooraj Dooba hai' #name of the song
+duration_seconds = 264 # Duration of the song in seconds
+rating = 4.5 # rating of the song
+
+print(artist)
+print(genre)
+print(song)
+print(duration_seconds)
+print(rating)
